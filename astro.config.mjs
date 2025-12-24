f2149cd756a054a28a3cc910a5d90f97db873eb1
thesoulpixel <thesoulpixel@gmail.com>
2025-12-25 03:23:25 +0530

    Add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://thetimesclock.com',
   prefetch: true,
+
   devToolbar: {
     enabled: false
   },
+
   integrations: [
     tailwind(),
     react(),
     sitemap()
   ],
+
+  adapter: cloudflare(),
 });
